6701b6dccbdf2b285fc153cea0f880bdf238d2ab
Azamat Suleymaov <a2amat@yandex.ru>
2021-06-10 19:09:31 +0500

    правила работают для всех языков

--- bind-rules.sh
+++ bind-rules.sh
@@ -2,7 +2,12 @@
 # Bind rules to project
 echo SQ version is:
 curl http://localhost:9000/api/server/version
-echo /n
+echo $'\n'
 echo Binding rules to project
 curl -u admin:admin -d 'project=prj&qualityProfile=GPN%20SVC%20way&language=cs' --request POST http://localhost:9000/api/qualityprofiles/add_project
-echo /n
+curl -u admin:admin -d 'project=prj&qualityProfile=GPN%20SVC%20way&language=go' --request POST http://localhost:9000/api/qualityprofiles/add_project
+curl -u admin:admin -d 'project=prj&qualityProfile=GPN%20SVC%20way&language=java' --request POST http://localhost:9000/api/qualityprofiles/add_project
+curl -u admin:admin -d 'project=prj&qualityProfile=GPN%20SVC%20way&language=js' --request POST http://localhost:9000/api/qualityprofiles/add_project
+curl -u admin:admin -d 'project=prj&qualityProfile=GPN%20SVC%20way&language=py' --request POST http://localhost:9000/api/qualityprofiles/add_project
+curl -u admin:admin -d 'project=prj&qualityProfile=GPN%20SVC%20way&language=ts' --request POST http://localhost:9000/api/qualityprofiles/add_project
+echo $'\n'
